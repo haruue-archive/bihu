9906dfd3baf16cb7c8b670f281cdc03d53100a70
Haruue Icymoon <haruue@caoyue.com.cn>
2016-01-23 02:58:42 +0800

    Launch page

---
+++ app/src/main/java/cn/com/caoyue/bihu/activity/LaunchPageActivity.java
@@ -0,0 +1,22 @@
+package cn.com.caoyue.bihu.activity;
+
+import android.annotation.SuppressLint;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+import cn.com.caoyue.bihu.R;
+
+public class LaunchPageActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_page);
+
+    }
+}
